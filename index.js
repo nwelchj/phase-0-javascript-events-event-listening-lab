e495f7cc715d7e325931e63a3db15313bfb3103b
Nolan Welch <bugygamer.mc@gmail.com>
2023-01-29 13:36:12 -0700

    commit tree 9f85fa62075980e2c325295df26f83703d1dda28

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const input = document.getElementById('button');
 function addingEventListener() {
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+      });
+    
 }
+
+
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+  input.addEventListener('click', clickAlert);
